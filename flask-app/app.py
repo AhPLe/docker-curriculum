0cae30387a75aa83bbb6ea6fefa06d26f9c2f2ef
AhPLe <arthurlebl+GitHub@gmail.com>
2022-02-22 14:53:00 -0500

    updated main

--- flask-app/app.py
+++ flask-app/app.py
@@ -67,5 +67,5 @@ def upload_file():
     # return render_template("index.html", url=url)
 
 
-# if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
