dbc29190ee38845d2632aad03e2c88b408218589
AhPLe <arthurlebl+GitHub@gmail.com>
2022-02-25 16:00:56 -0500

    Update grade_program.py

--- test/grade_program.py
+++ test/grade_program.py
@@ -6,12 +6,18 @@ def grade():
     subprocess.call("rm -f ./a.out", shell=True)
     retcode = subprocess.call("/usr/bin/g++ uploads/walk.cc", shell=True)
     if retcode:
+        compile = False
         print("failed to compile walk.cc")
         exit
+    else:
+        compile = True
     subprocess.call("rm -f ./output", shell=True)
     retcode = subprocess.call("./test.sh", shell=True)
-    grade = "Score: " + str(retcode) + " out of 2 correct."
-    print (grade)
+    if !compile:
+        grade = "Score: " + str(retcode) + " out of 2 correct, failed to compile."
+    else:
+        grade = "Score: " + str(retcode) + " out of 2 correct."
+    
     grade_output = "*************Original submission*************"
     grade = grade + '\n' + grade_output
     with open('uploads/walk.cc','r') as fs:
@@ -19,7 +25,7 @@ def grade():
         grade = grade + '\n' + grade_output
         #print(grade_output)
     
-    print(grade_output)
+    print(grade)
     
     #with open('graded.txt', 'wb') as fh:
     #    fh.write(grade)
